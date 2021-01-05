5a252a077a7bb37eb0acf413cb114d0b7c562bc9
Fernando Herrera <fernando.herrera85@gmail.com>
2021-01-05 15:43:03 -0500

    Fin sección 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pagina1Component,
+    MuestraNombreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
